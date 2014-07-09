91e248d212967c59efc41281a1d5a343a2399868
rezyah <rezafirmansyah@about.me>
2014-07-10 01:12:54 +0700

    Detail, Helper & routes

--- pkm/app/helpers.php
+++ pkm/app/helpers.php
@@ -1,3 +1,18 @@
 <?php
+	function beritaTag($Id) {
+		$Berita = Berita::findOrFail($Id);
+		$Return = null;
 
-	
+		foreach ($Berita->tag->all() as $count => $list) {
+			$Return .= $count == 0 ? '' : ', ';
+			if ($list->type == 'instansi') {
+				$Res = Instansi::findOrFail($list->tag_id);
+				$Return .= $Res->name;
+			} else {
+				$Res = Pelayanan::findOrFail($list->tag_id);
+				$Return .= $Res->name;
+			}
+		}
+
+		return $Return;
+	}
